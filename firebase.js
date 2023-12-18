3fcf4253ca40788710cac50a1aa705f86fee8312
babi2707 <90732683+babi2707@users.noreply.github.com>
2023-12-18 18:44:09 -0300

    cadastro funcionando

--- firebase.js
+++ firebase.js
@@ -1,21 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getFirestore, collection, addDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDtzrQhQHM5FxubxKvS0T2k8iGWwfrFKwc",
-  authDomain: "videochamada-30e6f.firebaseapp.com",
-  projectId: "videochamada-30e6f",
-  storageBucket: "videochamada-30e6f.appspot.com",
-  messagingSenderId: "605396899969",
-  appId: "1:605396899969:web:00211a5d72de6ac5189580",
-  measurementId: "G-VCF3ZBHE62"
+    apiKey: "AIzaSyD9JWrGO4TL7kud0nWmmN43xY7nQeneJgU",
+    authDomain: "videocall-bfd5a.firebaseapp.com",
+    databaseURL: "https://videocall-bfd5a-default-rtdb.firebaseio.com",
+    projectId: "videocall-bfd5a",
+    storageBucket: "videocall-bfd5a.appspot.com",
+    messagingSenderId: "43395021701",
+    appId: "1:43395021701:web:b3f8ce3fc1cc2a823922ec",
+    measurementId: "G-XP6CB37NND"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db, collection, addDoc, onSnapshot };
--- script.js
+++ script.js
@@ -1,24 +1,60 @@
 /*window.alert("Hello bitches!");
 window.confirm("Funcionou?");
 window.prompt("Qual o seu nome?");*/
-import { db, collection, addDoc, onSnapshot, doc, getDoc, updateDoc, deleteDoc } from "./firebase.js";
 
-function logar(){
-    var login = document.getElementById("login").value;
-    var senha = document.getElementById("senha").value;
+import { db, collection, addDoc, onSnapshot } from "./firebase.js";
 
-    if(login == "admin@gmail.com" && senha == "admin"){
-        alert("Sucesso!"); // mostra um alerta
-        location.href = "home.html"; //encaminha para a pag home
-    }else{
-        alert("Usuário ou senha incorretos!");
+window.onload = () => {
+
+    const formCadastro = document.getElementById("formC");
+    const formLogin = document.getElementById("formL");
+
+    formCadastro.onsubmit = async (event) => {
+        event.preventDefault();
+
+        let dados = {
+            nome: document.getElementById("nome").value,
+            email: document.getElementById("login").value,
+            senha: document.getElementById("senha").value
+        }
+
+        try {
+
+            await addDoc(collection(db, "videocall"), {
+              nome: dados.nome,
+              email: dados.email,
+              senha: dados.senha,
+            });
+      
+            alert("Usuário cadastrado com sucesso!");
+            location.href = "index.html";
+          } catch (error) {
+            console.error("Error adding document: ", error);
+          }
     }
 
-}
+    formLogin.onsubmit = async (event) => {
+        event.preventDefault();
 
-function cadastrar(){
-    var login = document.getElementById("login").value;
-    var login = document.getElementById("login").value;
-    var senha = document.getElementById("senha").value;
+        let input = {
+            email: document.getElementById("login").value,
+            senha: document.getElementById("senha").value
+        }
 
-}
+        onSnapshot(collection(db, "videocall"), (snapshot) => {
+            const items = snapshot.docs.data();
+
+            for (let i = 0; i < items.length; i++) {
+                const item = items[i];
+                if (item.email == input.email && item.senha == input.senha) {
+                    alert("Sucesso!");
+                    location.href = "home.html";
+                } else {
+                    alert("Usuário ou senha incorretos!");
+                }
+            }
+        });
+
+        
+    }
+};
